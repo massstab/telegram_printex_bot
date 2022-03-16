d0c5c072bebb94ebcec43edd1b7c82f654e6876e
massstab <dave@xiala.net>
2022-03-16 12:29:45 +0100

    Border cropped to border=1

--- src/to_latex.py
+++ src/to_latex.py
@@ -2,15 +2,15 @@ import subprocess
 import os
 
 def imagify(usercode):
-    head = r"\documentclass{standalone}\begin{document}"
+    head = r"\documentclass[varwidth, border=1]{standalone}" \
+           r"\begin{document}"
     tail = r"\end{document}"
     main = head + usercode + tail
-    print(os.getcwd())
     with open('tex/usercode.tex', 'w') as f:
         f.write(main)
     code = subprocess.call(['pdflatex', '-halt-on-error', '-output-directory', 'tex', 'tex/usercode.tex'])
     if code == 0:
-        subprocess.call(['convert', '-density', '600', '-bordercolor', 'white', '-border', '10', 'tex/usercode.pdf', 'tex/usercode.jpg'])
+        subprocess.call(['convert', '-density', '600', '-bordercolor', 'white', '-border', '0', 'tex/usercode.pdf', 'tex/usercode.jpg'])
         return True
     else:
         return False
